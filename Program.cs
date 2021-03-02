0f89ee97d08adddd35cb09295b97002dfadf9bbb
ApoloLopes <apoloaugusto10@hotmail.com>
2021-03-02 17:17:10 -0300

    implementando comunicacao modbusrtu

--- Program.cs
+++ Program.cs
@@ -9,44 +9,50 @@ using DAQPM5500.conections;
 
 namespace DAQPM5500
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            int milliseconds = 200;
 
-            ModbusClient modbusClient = new ModbusClient("169.254.0.10", 502);                                                //Defining ip adress and port 
-            modbusClient.Connect();                                                                                           //connecting to the server                                                
+            ConectionsType conect = new ConectionsType();
 
-            while (modbusClient.Connected)
-            {
+            //conect.ModbusTcp("169.254.0.10", 502);
+            conect.MdbusRtu("COM1", 1, 19200, "Even", "One");
+            
+            //int milliseconds = 200;
 
-                Console.Clear();
-                Console.WriteLine("o fabricante do multimedidor é " + modbusClient.ReadHoldingRegisters(70, 1)[0].ToString());
-                bool[] readCoils = modbusClient.ReadCoils(38415, 4);
+            //ModbusClient modbusClient = new ModbusClient("169.254.0.10", 502);                                                //Defining ip adress and port 
+            //modbusClient.Connect();                                                                                           //connecting to the server                                                
 
-                for (int i = 0; i < readCoils.Length; i++)
-                    Console.WriteLine("Status da entrada " + (i + 1) + " " + (38415 + i + 1) + " " + readCoils[i].ToString());
-                //console output
-                //for (int i = 0; i < readHoldingRegisters.Length; i++)
-                //    Console.WriteLine("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
-                //modbusClient.Disconnect();                                                                                       //Disconnect from Server
-                //Console.Write("Press any key to continue . . . ");
-                //    Console.WriteLine(modbusClient.ReadInputRegisters(39057, 1)[0].ToString());
+            //while (modbusClient.Connected)
+            //{
 
-                Console.WriteLine("voce está na subbranch");
-                Console.Write("Press any key to continue . . . ");
-                Console.ReadKey(true);
-                Thread.Sleep(milliseconds);
+            //    Console.Clear();
+            //    Console.WriteLine("o fabricante do multimedidor é " + modbusClient.ReadHoldingRegisters(70, 1)[0].ToString());
+            //    bool[] readCoils = modbusClient.ReadCoils(38415, 4);
+
+            //    for (int i = 0; i < readCoils.Length; i++)
+            //        Console.WriteLine("Status da entrada " + (i + 1) + " " + (38415 + i + 1) + " " + readCoils[i].ToString());
+            //console output
+            //for (int i = 0; i < readHoldingRegisters.Length; i++)
+            //    Console.WriteLine("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
+            //modbusClient.Disconnect();                                                                                       //Disconnect from Server
+            //Console.Write("Press any key to continue . . . ");
+            //    Console.WriteLine(modbusClient.ReadInputRegisters(39057, 1)[0].ToString());
+
+            //    Console.WriteLine("voce está na subbranch");
+            //    Console.Write("Press any key to continue . . . ");
+            //    Console.ReadKey(true);
+            //    Thread.Sleep(milliseconds);
 
 
-                //Console.ReadKey(true);
+            //Console.ReadKey(true);
 
-                //}
+            //}
 
-                //static void ServerPublishing()
-                //{
-            }
+            //static void ServerPublishing()
+            //{
+        
         }
     }
 }
--- conection/ConectionsType.cs
+++ conection/ConectionsType.cs
@@ -7,21 +7,92 @@ using EasyModbus;
 
 namespace DAQPM5500.conections
 {
-    class ConectionsType
+    public class ConectionsType
     {
 
-        private void modbusTcp(String ip, int port)
+        public void ModbusTcp(String ip, int port)
         {
             ModbusClient modbusClient = new ModbusClient(ip, port);
             try
             {
                 modbusClient.Connect();
                 Console.WriteLine("Client conected");
+                Console.ReadKey(true);
             }
 
             catch(SystemException)
             {
                 Console.WriteLine("Unable to conect to the client");
+                Console.ReadKey(true);
+            }
+
+        }
+
+        public void MdbusRtu(String port,  byte id = 1, int baudrate = 9600, String parity = "Even", String stopbits = "One")
+        {
+            ModbusClient modbusClient = new ModbusClient(port);
+            modbusClient.UnitIdentifier = id;
+            modbusClient.Baudrate = baudrate;
+
+            switch (parity)
+            {
+                case "Even":
+                    
+                    modbusClient.Parity = System.IO.Ports.Parity.Even;
+
+                    break;
+
+                case "Odd":
+
+                    modbusClient.Parity = System.IO.Ports.Parity.Odd;
+
+                    break;
+
+                default:
+
+                    modbusClient.Parity = System.IO.Ports.Parity.Even;
+
+                    break;
+            }
+
+            switch (stopbits)
+            {
+                case "Two":
+
+                    modbusClient.StopBits = System.IO.Ports.StopBits.Two;
+
+                    break;
+
+                case "OnePointFive":
+
+                    modbusClient.StopBits = System.IO.Ports.StopBits.OnePointFive;
+
+                    break;
+
+                case "One":
+
+                    modbusClient.StopBits = System.IO.Ports.StopBits.One;
+
+                    break;
+
+                default:
+
+                    modbusClient.StopBits = System.IO.Ports.StopBits.One;
+
+                    break;
+            }
+
+            try
+            {
+                modbusClient.Connect();
+                Console.WriteLine("Client conected");
+                Console.ReadKey(true);
+            }
+
+            catch (SystemException)
+            {
+                Console.WriteLine("Unable to conect to the client");
+                Console.ReadKey(true);
             }
 
         }

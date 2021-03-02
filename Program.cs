1fd1ac47d7a73011bc1d29907aa5050c4b6f81e8
ApoloLopes <apoloaugusto10@hotmail.com>
2021-03-02 14:20:19 -0300

    implementando a funcao de leitura das portas digitais

--- Program.cs
+++ Program.cs
@@ -10,36 +10,39 @@ namespace DAQPM5500
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int milliseconds = 200;
+
             ModbusClient modbusClient = new ModbusClient("169.254.0.10", 502);                                                //Defining ip adress and port 
             modbusClient.Connect();                                                                                           //connecting to the server                                                
 
-           
             while (modbusClient.Connected)
             {
+
                 Console.Clear();
-                //Console.WriteLine("o fabricante do multimedidor é " + modbusClient.ReadHoldingRegisters(70, 1)[0].ToString());
+                Console.WriteLine("o fabricante do multimedidor é " + modbusClient.ReadHoldingRegisters(70, 1)[0].ToString());
                 bool[] readCoils = modbusClient.ReadCoils(38415, 4);
 
                 for (int i = 0; i < readCoils.Length; i++)
-                    Console.WriteLine("Value of Coil " + (38415 + i + 1) + " " + readCoils[i].ToString());
+                    Console.WriteLine("Status da entrada " + (i + 1) + " " + (38415 + i + 1) + " " + readCoils[i].ToString());
                 //console output
                 //for (int i = 0; i < readHoldingRegisters.Length; i++)
                 //    Console.WriteLine("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
                 //modbusClient.Disconnect();                                                                                       //Disconnect from Server
                 //Console.Write("Press any key to continue . . . ");
+                //    Console.WriteLine(modbusClient.ReadInputRegisters(39057, 1)[0].ToString());
+
                 Thread.Sleep(milliseconds);
-                
+
+
+                //Console.ReadKey(true);
+
+                //}
+
+                //static void ServerPublishing()
+                //{
             }
-            //Console.ReadKey(true);
-
-        }
-
-        static void ServerPublishing()
-        {
-
         }
     }
 }
---
+++ conection/ConectionsType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyModbus;
+
+namespace DAQPM5500.conections
+{
+    class ConectionsType
+    {
+
+        private void modbusTcp(String ip, int port)
+        {
+            ModbusClient modbusClient = new ModbusClient(ip, port);
+            try
+            {
+                modbusClient.Connect();
+                Console.WriteLine("Client conected");
+            }
+
+            catch(SystemException)
+            {
+                Console.WriteLine("Unable to conect to the client");
+            }
+
+        }
+
+    }
+}

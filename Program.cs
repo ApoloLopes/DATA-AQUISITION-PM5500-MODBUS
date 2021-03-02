53e2cfffc797efe57ddb13e6ec042150f884b7d3
ApoloLopes <apoloaugusto10@hotmail.com>
2021-03-02 08:53:09 -0300

    terminando de implementar a leitura das entradas digitais

--- Program.cs
+++ Program.cs
@@ -12,12 +12,15 @@ namespace DAQPM5500
         {
             ModbusClient modbusClient = new ModbusClient("169.254.0.10", 502);                                                //Defining ip adress and port 
             modbusClient.Connect();                                                                                           //connecting to the server                                                
-            
-            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);
-            
+
+            //Console.WriteLine("o fabricante do multimedidor é " + modbusClient.ReadHoldingRegisters(70, 1)[0].ToString());
+            bool[] readCoils = modbusClient.ReadCoils(38415, 4);
+
+            for (int i = 0; i < readCoils.Length; i++)
+                Console.WriteLine("Value of Coil " + (38415 + i + 1) + " " + readCoils[i].ToString());
             //console output
-            for (int i = 0; i < readHoldingRegisters.Length; i++)
-                Console.WriteLine("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
+            //for (int i = 0; i < readHoldingRegisters.Length; i++)
+            //    Console.WriteLine("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
             modbusClient.Disconnect();                                                                                       //Disconnect from Server
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);

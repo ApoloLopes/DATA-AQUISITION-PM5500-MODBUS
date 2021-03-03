3d3c40923891af6ab0fe7e2942639d9a1fba5c8e
ApoloLopes <apoloaugusto10@hotmail.com>
2021-03-03 09:32:47 -0300

    implementando trabalho com threads

--- Program.cs
+++ Program.cs
@@ -11,13 +11,34 @@ namespace DAQPM5500
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            ConectionsType conect = new ConectionsType();
+        static ConectionsType conect = new ConectionsType();
+        private static String ip = "169.254.0.10";
+        private static int port = 502;
+        private static String comPort = "COM1";
+        private static byte id = 1;
+        private static int baudrate = 19200;
+        private static String parity = "Even";
+        private static String stopbits = "One";
 
-            conect.ModbusTcp("169.254.0.10", 502);
-            //conect.MdbusRtu("COM1", 1, 19200, "Even", "One");
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(ModbusTcp);
+            t.Start();
+            //conect.ModbusTcp("169.254.0.10", 502);
+            ModbusRtu();
+
+            t.Abort();
    
+        }
+
+        static void ModbusTcp()
+        {
+            conect.ModbusTcp(ip, port);
+        }
+
+        static void ModbusRtu()
+        {
+            conect.MdbusRtu(comPort, id, baudrate, parity, stopbits);
         }
     }
 }
